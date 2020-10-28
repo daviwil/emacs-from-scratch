10c1efd4f138ba83b9057d8483fec42514f1c8fa
David Wilson <david@daviwil.com>
2020-10-28 08:08:38 -0700

    Set Emacs configuration directory in stream-tweaks.el

--- stream-tweaks.el
+++ stream-tweaks.el
@@ -1,8 +1,9 @@
 ;; This file has configuration tweaks that are only meant to be used in Emacs
 ;; From Scratch streams.
 
-;; Don't use my own package folder by accident
-(setq package-user-dir "~/Projects/Code/emacs-from-scratch/packages")
+;; Use this folder as the Emacs configuraton folder
+(setq user-emacs-directory "~/Projects/Code/emacs-from-scratch/")
+(customize-set-value 'custom-theme-directory user-emacs-directory)
 
 (setq efs/default-font-size 300)
 (setq efs/default-variable-font-size 340)
