eb1a1e96c3d8626f84d5386fe6b5479cca27babd
David Wilson <david@daviwil.com>
2020-12-02 10:34:11 -0800

    Add configuration and show notes for Emacs IDE 2 - Python

--- init.el
+++ init.el
@@ -404,9 +404,23 @@
 (use-package typescript-mode
   :mode "\\.ts\\'"
   :hook (typescript-mode . lsp-deferred)
-
   :config
   (setq typescript-indent-level 2))
+
+(use-package python-mode
+  :ensure t
+  :hook (python-mode . lsp-deferred)
+  :custom
+  ;; NOTE: Set these if Python 3 is called "python3" on your system!
+  ;; (python-shell-interpreter "python3")
+  ;; (dap-python-executable "python3")
+  (dap-python-debugger 'debugpy)
+  :config
+  (require 'dap-python))
+
+(use-package pyvenv
+  :config
+  (pyvenv-mode 1))
 
 (use-package company
   :after lsp-mode
